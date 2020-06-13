d63f0395610c45e3ce788a2a6752cb33204285eb
blacqvve <44911178+blacqvve@users.noreply.github.com>
2020-06-13 15:34:11 +0300

    init

---
+++ Assets/Scripts/Hexagon/Bomb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bomb : Hexagon
+{
+     [SerializeField]
+     private int timer;
+     [SerializeField]
+     private Text text;
+     private void Awake()
+     {
+          gridManager = GridManager.Instance;
+          text.text = timer.ToString();
+     }
+     private void OnEnable()
+     {
+          gridManager = GridManager.Instance;
+          timer = HexMetrics.BOMB_TIMER;
+          text.text = timer.ToString();
+          gridManager.OnMoveMade += HandleTimer;
+     }
+     private void HandleTimer()
+     {
+
+               timer--;
+               if (timer == 0)
+               {
+               MenuManager.Instance.GameOver();
+               }
+               text.text = timer.ToString();  
+     }
+
+     private void OnDisable()
+     {
+          gridManager.OnMoveMade -= HandleTimer;
+          gridManager = null;
+     }
+
+}
